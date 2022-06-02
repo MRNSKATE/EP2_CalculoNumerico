70210da20497e0b5e367cc0a985ffc9c7c52b234
MarcoAntonioRudasNapoli <marco.rudas@polijunior.com.br>
2022-06-02 10:43:21 -0300

    add: Error Value Result

--- index.py
+++ index.py
@@ -1,5 +1,5 @@
 from math import *
-from data import datas
+from data import final_calculate
 
 names = {'sqrt': sqrt, 'pow': pow}
 
@@ -16,11 +16,9 @@ for i in range(2):
         border = "superior"
     value_gap = float(input(f'Digite o intervalo {border} mais externo: '))
     x_gap.append(value_gap)
-#Obtendo x_gap negativo
-x_gap_negative=[]
-for i in range(len(x_gap)):
-    x_gap_new = (-1.0)*float(x_gap[i])
-    x_gap_negative.append(x_gap_new)
+
+x_sup = x_gap[1]
+x_inf = x_gap[0]
 #Mapeamento do intervalo y
 y_gap = []
 for i in range(2):
@@ -30,11 +28,8 @@ for i in range(2):
         border = "superior"
     value_gap = input(f'Digite o intervalo {border} mais externo: ')
     y_gap.append(value_gap)
-#Obtendo y_gap negativo
-y_gap_negative=[]
-for i in range(len(y_gap)):
-    y_gap_new = "-"+y_gap[i]
-    y_gap_negative.append(y_gap_new)
+y_sup = y_gap[1]
+y_inf = y_gap[0]
 #Mapeando o valor de yi e substituindo na fórmula
 if n == 6:
     x_j =  [0.2386191860831969086305017, 0.6612093864662645136613996, 0.9324695142031520278123016]
@@ -52,21 +47,21 @@ while i < n:
     if i < n/2:
         loop_x_j = str(x_j[i])
         loop_w_j = str(w_j[i])
-        x_i = "(1/2)*((y_gap[1] + y_gap_negative[0])*"+loop_x_j+"+(y_gap[1] + y_gap[0]))"
-        newfunction = function.replace('y', x_i)+"*"+loop_w_j+"*(y_gap[1] + y_gap_negative[0])/2"
+        x_i = "(1/2)*((y_sup - y_inf)*"+loop_x_j+"+(y_sup + y_inf))"
+        newfunction = function.replace('y', x_i)+"*"+loop_w_j+"*(y_sup - y_inf)/2"
         sum_functions_y.append(newfunction)
     else:
         index = int((n/2)-i-1)
         loop_x_j = str(x_j[index])
         loop_w_j = str(w_j[index])
 
-        x_i = "(1/2)*((y_gap[1] + y_gap_negative[0])*"+loop_x_j+"+(y_gap[1] + y_gap[0]))"
-        newfunction = function.replace('y', x_i)+"*"+loop_w_j+"*(y_gap[1] + y_gap_negative[0])/2"
+        x_i = "(1/2)*((y_sup - y_inf)*"+loop_x_j+"+(y_sup + y_inf))"
+        newfunction = function.replace('y', x_i)+"*"+loop_w_j+"*(y_sup - y_inf)/2"
         sum_functions_y.append(newfunction)
     i = i + 1
 
 #Mapeando o valor de xi e substituindo na fórmula
-
+x_j_map = []
 sum_functions_x = []
 for j in range(len(sum_functions_y)):
     i=0
@@ -74,29 +69,27 @@ for j in range(len(sum_functions_y)):
         if i < n/2:
             loop_x_j = str(x_j[i])
             loop_w_j = str(w_j[i])
-            x_i = "(1/2)*((x_gap[1] + x_gap_negative[0])*"+loop_x_j+"+(x_gap[1] + x_gap[0]))"
-            newfunction = sum_functions_y[i].replace('x', x_i)+"*"+loop_w_j+"*(x_gap[1] + x_gap_negative[0])/2"
+            x_i = "(1/2)*((x_sup - x_inf)*"+loop_x_j+"+(x_sup + x_inf))"
+            newfunction = sum_functions_y[i].replace('x', x_i)+"*"+loop_w_j+"*(x_sup - x_inf)/2"
             sum_functions_x.append(newfunction)
+            x_j_map.append(float(loop_x_j))
         else:
             index = int((n/2)-i-1)
             loop_x_j = str(x_j[index])
             loop_w_j = str(w_j[index])
 
-            x_i = "(1/2)*((x_gap[1] + x_gap_negative[0])*"+loop_x_j+"+(x_gap[1] + x_gap[0]))"
-            newfunction = sum_functions_y[index].replace('x', x_i)+"*"+loop_w_j+"*(x_gap[1] + x_gap_negative[0])/2"
+            x_i = "(1/2)*((x_sup - x_inf)*"+loop_x_j+"+(x_sup + x_inf))"
+            newfunction = sum_functions_y[index].replace('x', x_i)+"*"+loop_w_j+"*(x_sup - x_inf)/2"
             sum_functions_x.append(newfunction)
+            x_j_map.append(float(loop_x_j))
         i = i + 1
 
-def calculando_final(sum_functions_x, x_gap, y_gap):
-    somatorio = 0
-    for i in range(len(sum_functions_x)):
-        #somatorio = somatorio + eval(sum_functions_x[i])
-        print(eval(sum_functions_x[i]))
-    return somatorio
-print(sum_functions_x)
-print(x_gap)
-print(y_gap)
-print(calculando_final(sum_functions_x, x_gap, y_gap))
-'''for i in range(len(sum_functions_x)):
-    number = eval(sum_functions_x[i])
-    print(number)'''
+geral_result = 0
+for i in range(len(sum_functions_x)):
+    f_calc = sum_functions_x[i]
+
+    geral_result = geral_result + final_calculate(x_sup, x_inf, y_sup, y_inf, f_calc, x_j_map[i], n)
+    print(f_calc)
+    print('\n')
+
+print(geral_result)
